4b88abdad1b5c901cd68d65f90333baacd7b0191
Nitish Joshi <joshinh@gmail.com>
2017-10-25 23:41:07 +0530

    Updated number of clusters

--- document_cluster.py
+++ document_cluster.py
@@ -5,17 +5,22 @@ import random
 import warnings
 from utils import preprocess
 from collections import Counter
+from math import sqrt
 
 def k_means(setd):
 	setd = preprocess([" ".join(y) for y in setd])
-	num_cluster = 2
+	
+	num_cluster = max(2,int(sqrt(len(setd)/2))+1)
+	
 	word_dict = form_dict(setd)
 	inp_vector = np.zeros((len(setd),len(word_dict)))
 	for p1,doc in enumerate(setd):
 		for p2,word in enumerate(word_dict):
 			inp_vector[p1][p2] = calctfidf(setd,doc,word)
+	
 	cluster_vector = np.zeros((num_cluster,len(word_dict)))
 	cluster_master = []
+	
 	for t in range(50):
 		rand_init = random.sample(range(len(setd)),num_cluster)
 		cluster_vector = inp_vector[rand_init , :]
@@ -31,14 +36,10 @@ def k_means(setd):
 					warnings.simplefilter("ignore", category=RuntimeWarning)
 					cluster_vector[j] = np.mean(cluster_alloc,axis=0)	
 		cluster_master.append(cluster_assign.tolist())
+	
 	cluster_list = Counter(map(tuple,cluster_master))
 	cluster_final = cluster_list.most_common(1) 
-	return list(list(cluster_final[0])[0])
-
-
-# print(k_means(preprocess(doclist)))
-
-
-
-
 	
+	return list(list(cluster_final[0])[0]),num_cluster
+
+# print(k_means(a))
